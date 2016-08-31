055f8c8daac43ca979e1e653db4ab6c7bc6bce2a
torii_y <torii_y@worksap.co.jp>
2016-08-31 15:58:39 +0900

    fix edge cases

--- src/HtmlParser.elm
+++ src/HtmlParser.elm
@@ -48,12 +48,12 @@ tagName =
 
 attributeName : Parser String
 attributeName =
-  regex "[a-zA-Z][a-zA-Z\\-]*"
+  regex "[a-zA-Z][a-zA-Z:\\-]*"
 
 
 attributeValueNumber : Parser AttributeValue
 attributeValueNumber =
-  map NumberValue (regex "[1-9][0-9.]*")
+  map NumberValue (regex "[0-9][0-9.]*")
 
 
 attributeValueString : Parser AttributeValue
@@ -64,7 +64,7 @@ attributeValueString =
 
 attributeValueBareString : Parser AttributeValue
 attributeValueBareString =
-  map StringValue (regex "[a-zA-Z]+")
+  map StringValue (regex "[a-zA-Z0-9\\-]+")
 
 
 attributeValue : Parser AttributeValue
--- test/Tests.elm
+++ test/Tests.elm
@@ -45,6 +45,7 @@ nodeTests : Test
 nodeTests =
   suite "Node"
     [ test "basic" (testParse "<a></a>" (Node "a" [] []))
+    , test "basic" (testParse " <a></a> " (Node "a" [] []))
     , test "basic" (testParse "<A></A>" (Node "a" [] []))
     , test "basic" (testParse "<a>a</a>" (Node "a" [] [ Text "a" ]))
     , test "basic" (testParse "<a> a </a>" (Node "a" [] [ Text " a " ]))
@@ -75,6 +76,8 @@ nodeTests =
     , test "optional-end-tag" (testParse "<tbody><tr><td><tr><td></tbody>" (Node "tbody" [] [ Node "tr" [] [ Node "td" [] [] ], Node "tr" [] [ Node "td" [] [] ] ]))
     , test "optional-end-tag" (testParse "<tbody><tr><th><td><tr><th><td></tbody>" (Node "tbody" [] [ Node "tr" [] [ Node "th" [] [], Node "td" [] [] ], Node "tr" [] [ Node "th" [] [], Node "td" [] [] ] ]))
     , test "optional-end-tag" (testParse "<table><tr><td><tr><td></table>" (Node "table" [] [ Node "tr" [] [ Node "td" [] [] ], Node "tr" [] [ Node "td" [] [] ] ]))
+    -- , test "optional-end-tag" (testParse "<table><tr><!----><tr></table>" (Node "table" [] [ Node "tr" [] [], Comment "", Node "tr" [] [] ]))
+    -- , test "optional-end-tag" (testParse "<table><tr><tr><!----></table>" (Node "table" [] [ Node "tr" [] [], Node "tr" [] [], Comment "" ]))
     ]
 
 
@@ -100,6 +103,8 @@ attributeTests =
     , test "basic" (testParse """<input max=100 min = 10.5/>""" (Node "input" [("max", NumberValue "100"), ("min", NumberValue "10.5")] []))
     , test "basic" (testParse """<input disabled>""" (Node "input" [("disabled", NoValue)] []))
     , test "basic" (testParse """<input disabled/>""" (Node "input" [("disabled", NoValue)] []))
+    , test "basic" (testParse """<meta http-equiv=Content-Type>""" (Node "meta" [("http-equiv", StringValue "Content-Type")] []))
+    , test "basic" (testParse """<html xmlns:v="urn:schemas-microsoft-com:vml"></html>""" (Node "html" [("xmlns:v", StringValue "urn:schemas-microsoft-com:vml")] []))
     ]
 
 
@@ -107,6 +112,8 @@ intergrationTests : Test
 intergrationTests =
   suite "Integration"
     [ test "table" (testParseComplex ["table", "caption", "colgroup", "col", "thead", "tbody", "tr", "th", "td"] [] fullOmission)
+    , test "table" (testParseComplex ["body", "table", "col", "tr", "td"] [] clipboardFromExcel2013)
+    , test "table" (testParseComplex ["body", "table", "col", "tr", "td"] [] clipboardFromOpenOfficeCalc)
     ]
 
 
@@ -124,6 +131,92 @@ fullOmission = """
     <tr> <td>Engineer's cab lighting               <td>                 <td>✔
     <tr> <td>Station Announcements - Swiss         <td>                 <td>✔
   </table>
+  """
+
+
+clipboardFromExcel2013 : String
+clipboardFromExcel2013 = """
+  <body link="#0563C1" vlink="#954F72">
+
+  <table border=0 cellpadding=0 cellspacing=0 width=216 style='border-collapse:
+   collapse;width:162pt'>
+  <!--StartFragment-->
+   <col width=72 span=3 style='width:54pt'>
+   <tr height=18 style='height:13.5pt'>
+    <td height=18 align=right width=72 style='height:13.5pt;width:54pt'>1</td>
+    <td align=right width=72 style='width:54pt'>2</td>
+    <td align=right width=72 style='width:54pt'>3</td>
+   </tr>
+   <tr height=18 style='height:13.5pt'>
+    <td height=18 class=xl69 align=right style='height:13.5pt'>2</td>
+    <td class=xl66 align=right>3</td>
+    <td align=right>4</td>
+   </tr>
+   <tr height=18 style='height:13.5pt'>
+    <td height=18 class=xl65 align=right style='height:13.5pt'>3</td>
+    <td class=xl66 align=right>4</td>
+    <td class=xl65 align=right>5</td>
+   </tr>
+   <tr height=18 style='height:13.5pt'>
+    <td height=18 class=xl65 align=right style='height:13.5pt'>4</td>
+    <td class=xl66 align=right>5</td>
+    <td class=xl65 align=right>6</td>
+   </tr>
+   <tr height=18 style='height:13.5pt'>
+    <td height=18 class=xl67 align=right style='height:13.5pt'>5</td>
+    <td class=xl67 align=right>6</td>
+    <td class=xl67 align=right>7</td>
+   </tr>
+   <tr height=18 style='height:13.5pt'>
+    <td height=18 class=xl68 align=right style='height:13.5pt'>6</td>
+    <td class=xl68 align=right>7</td>
+    <td class=xl68 align=right>8</td>
+   </tr>
+  <!--EndFragment-->
+  </table>
+
+  </body>
+  """
+
+clipboardFromOpenOfficeCalc : String
+clipboardFromOpenOfficeCalc = """
+  <BODY TEXT="#000000">
+  <TABLE FRAME=VOID CELLSPACING=0 COLS=3 RULES=NONE BORDER=0>
+    <COLGROUP><COL WIDTH=86><COL WIDTH=86><COL WIDTH=86></COLGROUP>
+      <TBODY>
+          <TR>
+              <TD WIDTH=86 HEIGHT=19 ALIGN=RIGHT VALIGN=MIDDLE SDVAL="1" SDNUM="1041;"><FONT COLOR="#000000">1</FONT></TD>
+              <TD WIDTH=86 ALIGN=CENTER VALIGN=MIDDLE SDVAL="2" SDNUM="1041;"><FONT COLOR="#000000">2</FONT></TD>
+              <TD WIDTH=86 ALIGN=RIGHT VALIGN=MIDDLE SDVAL="3" SDNUM="1041;"><FONT COLOR="#000000">3</FONT></TD>
+          </TR>
+          <TR>
+              <TD HEIGHT=19 ALIGN=LEFT VALIGN=MIDDLE SDVAL="2" SDNUM="1041;"><FONT COLOR="#000000">2</FONT></TD>
+              <TD ALIGN=CENTER VALIGN=MIDDLE SDVAL="3" SDNUM="1041;"><B><FONT COLOR="#000000">3</FONT></B></TD>
+              <TD ALIGN=RIGHT VALIGN=MIDDLE SDVAL="4" SDNUM="1041;"><B><FONT COLOR="#000000">4</FONT></B></TD>
+          </TR>
+          <TR>
+              <TD HEIGHT=19 ALIGN=LEFT VALIGN=MIDDLE BGCOLOR="#FFFF00" SDVAL="3" SDNUM="1041;"><FONT COLOR="#000000">3</FONT></TD>
+              <TD ALIGN=CENTER VALIGN=MIDDLE BGCOLOR="#FFFF00" SDVAL="4" SDNUM="1041;"><B><FONT COLOR="#000000">4</FONT></B></TD>
+              <TD ALIGN=RIGHT VALIGN=MIDDLE BGCOLOR="#FFFF00" SDVAL="5" SDNUM="1041;"><B><FONT COLOR="#000000">5</FONT></B></TD>
+          </TR>
+          <TR>
+              <TD HEIGHT=19 ALIGN=LEFT VALIGN=MIDDLE BGCOLOR="#FFFF00" SDVAL="4" SDNUM="1041;"><FONT COLOR="#000000">4</FONT></TD>
+              <TD ALIGN=CENTER VALIGN=MIDDLE BGCOLOR="#FFFF00" SDVAL="5" SDNUM="1041;"><FONT COLOR="#000000">5</FONT></TD>
+              <TD ALIGN=RIGHT VALIGN=MIDDLE BGCOLOR="#FFFF00" SDVAL="6" SDNUM="1041;"><FONT COLOR="#000000">6</FONT></TD>
+          </TR>
+          <TR>
+              <TD HEIGHT=19 ALIGN=LEFT VALIGN=MIDDLE BGCOLOR="#FFFF00" SDVAL="5" SDNUM="1041;"><FONT COLOR="#000000">5</FONT></TD>
+              <TD ALIGN=CENTER VALIGN=MIDDLE BGCOLOR="#FFFF00" SDVAL="6" SDNUM="1041;"><FONT COLOR="#000000">6</FONT></TD>
+              <TD ALIGN=RIGHT VALIGN=MIDDLE BGCOLOR="#FFFF00" SDVAL="7" SDNUM="1041;"><FONT COLOR="#000000">7</FONT></TD>
+          </TR>
+          <TR>
+              <TD HEIGHT=19 ALIGN=RIGHT VALIGN=MIDDLE SDVAL="6" SDNUM="1041;"><FONT COLOR="#000000">6</FONT></TD>
+              <TD ALIGN=CENTER VALIGN=MIDDLE SDVAL="7" SDNUM="1041;"><FONT COLOR="#000000">7</FONT></TD>
+              <TD ALIGN=RIGHT VALIGN=MIDDLE SDVAL="8" SDNUM="1041;"><FONT COLOR="#000000">8</FONT></TD>
+          </TR>
+      </TBODY>
+  </TABLE>
+  </BODY>
   """
 
 
